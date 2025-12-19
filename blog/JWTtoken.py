0054df46923752b39b8cf6dd78df2019e401efda
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-19 13:40:03 +0200

    Fix environment variables

--- blog/JWTtoken.py
+++ blog/JWTtoken.py
@@ -2,10 +2,14 @@ from datetime import datetime, timedelta, timezone
 import jwt
 from jwt.exceptions import InvalidTokenError
 from . import schemas
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECTRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
