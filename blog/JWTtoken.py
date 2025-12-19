e874bc0c946ff342867eccf9293d21a74041065e
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-19 13:48:27 +0200

    Update environment variables

--- blog/JWTtoken.py
+++ blog/JWTtoken.py
@@ -7,9 +7,9 @@ import os
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECTRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
